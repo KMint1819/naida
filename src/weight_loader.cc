5fcda9f88c9e4045c757b04e8a82381e94e274e2
kmint <kmint1819@gmail.com>
2026-04-04 18:48:11 -0700

    can load weights

--- src/weight_loader.cc
+++ src/weight_loader.cc
@@ -2,6 +2,8 @@
 #include "naida/common.hh"
 
 #include <fstream>
+#include <memory>
+#include <cstring>
 
 namespace naida
 {
@@ -15,6 +17,19 @@ std::vector<std::byte> WeightLoader::load_layer(const std::string_view &name)
 {
     std::vector<std::byte> buf = read_binary_file(weight_path);
     printf("%d\n", buf.size());
+
+    uint64_t len_header = 0;
+    std::memcpy(&len_header, buf.data(), 8);
+    printf("header size: %ld\n", len_header);
+
+    std::vector<char> json_buf;
+    json_buf.resize(len_header);
+
+    std::memcpy(json_buf.data(), buf.data() + 8, len_header);
+
+    printf("json: %s\n", json_buf.data());
+
+
     return buf;
 }
 } // namespace naida
